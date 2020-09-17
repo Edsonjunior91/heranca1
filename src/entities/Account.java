f746499cc0db6cde667e8b45a3743d855e429b3d
Edson Junior <edson_junior91@hotmail.com>
2020-09-17 00:06:56 -0300

    Commit final Herança

---
+++ src/entities/Account.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class Account {
+	private Integer number;
+	private String holder;
+	private Double balance;
+	
+	public Account() {
+		
+	}
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+	public String getHolder() {
+		return holder;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public void setHolder(String Holder) {
+		this.holder = holder;
+	}
+	public void withdraw(double amount) {
+		balance =- amount;
+	}
+	public void deposit(double amount) {
+		balance =- amount;
+	}
+}
